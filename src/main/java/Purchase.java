f02e270f91c5084d77a9c16207ffc1a676d95a13
danoon3 <danoone58@gmail.com>
2023-04-20 11:33:49 +0300

    new exercise

---
+++ src/main/java/Purchase.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+public class Purchase {
+    protected String title;
+    protected int count;
+
+    public Purchase(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+
+    public Purchase() {
+    }
+
+}
